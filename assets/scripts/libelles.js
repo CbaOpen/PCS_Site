8a59c005b2b1d242bb8eea0ac7ab60ea79d511e2
Claire Baskevitch <user@debian.arcahe.ovh>
2019-07-09 07:20:40 +0200

    ajout mots non signifiants : non fonctionnel pour le moment

--- assets/scripts/libelles.js
+++ assets/scripts/libelles.js
@@ -40,12 +40,9 @@ function autocomplete(inp, arr) {
         this.parentNode.appendChild(a);
         /*for each item in the array...*/
         var valSplit = arr[0].split(' ')
-  //        console.log(valSplit)
 
         for (i = 0; i < arr.length; i++) {
           valSplit = arr[i].split(' ')
-          //console.log(valSplit)
-          //console.log(valSplit.length)
           /*check if the item starts with the same letters as the text field value:*/
           if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
             /*create a DIV element for each matching element:*/
@@ -68,7 +65,7 @@ function autocomplete(inp, arr) {
           else if(valSplit.length > 1){
             //console.log("here")
             for(j=1; j<valSplit.length; j++){
-              if(valSplit[j].substr(0, val.length).toUpperCase() == val.toUpperCase()){
+              if(/*is_significant(valSplit[j], non_signif_words) && */valSplit[j].substr(0, val.length).toUpperCase() == val.toUpperCase()){
                 /*create a DIV element for each matching element:*/
                 b = document.createElement("DIV");
                 /* Write words before matching element */
@@ -80,6 +77,16 @@ function autocomplete(inp, arr) {
                 b.innerHTML += valSplit[j].substr(val.length) + " ";
                 for(k=j+1; k<valSplit.length; k++)
                   b.innerHTML += valSplit[k] + " ";
+                /*insert a input field that will hold the current array item's value:*/
+                b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+                /*execute a function when someone clicks on the item value (DIV element):*/
+                b.addEventListener("click", function(e) {
+                    /*insert the value for the autocomplete text field:*/
+                    inp.value = this.getElementsByTagName("input")[0].value;
+                    /*close the list of autocompleted values,
+                    (or any other open lists of autocompleted values:*/
+                    closeAllLists();
+                });
                 a.appendChild(b);
                 break;
               }
@@ -145,6 +152,16 @@ function autocomplete(inp, arr) {
   });
 }
 
-var testArr = ["FORREUR", "AIDE FORREUR"]
+var non_signif_words = ["À","AU","D'UN","DANS","DE","DES","DU","EN","ET","LA","LE","OU","SUR","AUX","POUR","AVEC", "CHEZ","D’UNE","D’","L’","PAR",
+                      "(",")","/","-"]
+
+function is_significant(word, tab_non_signif){
+  for(var i=0; i<tab_non_signif.length; i++){
+    if(tab_non_signif[i] == word.toUpperCase())
+      return false;
+  }
+  return true;
+}
+var testArr = ["est aux miel", "test"]
 /*initiate the autocomplete function on the "myInput" element, and pass along the countries array as possible autocomplete values:*/
 autocomplete(document.getElementById("myInput"), libprof);
